85fbfdca2ddd40ca37e5c589937a8537b851bc8d
Maras59 <marcosespino103@gmail.com>
2025-04-23 16:18:37 -0700

    Implement video playback

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # BadApplePlayer
-A simple program that plays Bad Apple in your terminal
+A simple program that plays Bad Apple in your Linux terminal, currently only tested in ubuntu
 
 ## Running The Program
 Ensure you distro is up to date and you have cmake installed:
+
 `sudo apt update && sudo apt install -y cmake`
 
 Install OpenCV:
+
 `sudo apt install libopencv-dev`
 
 Create a `build` directory if one does not exist. Inside of the build directory run the following commands:
--- main.cpp
+++ main.cpp
@@ -8,63 +8,80 @@
 using namespace std;
 using namespace cv;
  
+
+const string ASCII_CHARS = " .:-=+*#%@";
+
+char getAsciiChar(uchar intensity) {
+    size_t index = (intensity * (ASCII_CHARS.length() - 1)) / 255;
+    return ASCII_CHARS[index];
+}
+
 int main(){
-  // Go up a level from "build" directory 
-  VideoCapture cap("../BadApple.mp4"); 
-  if(!cap.isOpened()){
-    cout << "Error opening video stream or file" << endl;
-    return -1;
-  }
-  // Initialize ncurses
-  initscr();
-  noecho();
-  cbreak();
-
-  curs_set(0);
-  WINDOW *win = newwin(LINES - 2, COLS - 2, 1, 1);
-
-  // Get video aspect ratio from first frame
-  int width = static_cast<int>(cap.get(CAP_PROP_FRAME_WIDTH));
-  int height = static_cast<int>(cap.get(CAP_PROP_FRAME_HEIGHT));
-
-  int scaled_w;
-  int scaled_h;
-
-  double aspect_ratio = height / (width * 2.5); // TODO: Does this need to be hardcoded? Is there a better number to account for char height? 
-
-  scaled_w = min(width, COLS - 2);
-  scaled_h = int(static_cast<double>(width) * aspect_ratio);
-  if (scaled_h > LINES - 2) {
-    // If the scaled height exceeds max height, scale based on height instead
-    scaled_h = min(height, LINES - 2);
-    scaled_w = static_cast<int>(static_cast<double>(scaled_h) / aspect_ratio);
-  }
-
-
-  while(1){
- 
-    Mat frame;
-    cap >> frame;
-    if (frame.empty())
-      break;
-    
-    vector<uchar> array;
-    if (frame.isContinuous()) {
-      array.assign(frame.data, frame.data + frame.total()*frame.channels());
+    // Go up a level from "build" directory 
+    VideoCapture cap("../BadApple.mp4"); 
+    if(!cap.isOpened()){
+        cout << "Error opening video stream or file" << endl;
+        return -1;
     }
+    // Initialize ncurses
+    initscr();
+    noecho();
+    cbreak();
+    curs_set(0);
 
-    // represent data in ascii and print to screen.
+    WINDOW *win = newwin(LINES - 2, COLS - 2, 1, 1);
 
+    // Get video aspect ratio from first frame
+    int width = static_cast<int>(cap.get(CAP_PROP_FRAME_WIDTH));
+    int height = static_cast<int>(cap.get(CAP_PROP_FRAME_HEIGHT));
 
+    int scaled_w;
+    int scaled_h;
 
- 
-    char c=(char)waitKey(25);
-    if(c==27)
-      break;
-  }
-  
-  cap.release();
-  destroyAllWindows();
-  
-  return 0;
+    double aspect_ratio = height / (width * 2.5); // TODO: Does this need to be hardcoded? Is there a better number to account for char height? 
+
+    while(1){
+        Mat o_frame, rs_frame, f_frame; // Original, resized, final
+        cap >> o_frame;
+        if (o_frame.empty())
+            break;
+        
+        // Recalculate every frame to allow dynamic resizing
+        scaled_w = min(width, COLS - 2);
+        scaled_h = int(static_cast<double>(width) * aspect_ratio);
+        if (scaled_h > LINES - 2) {
+            // If the scaled height exceeds max height, scale based on height instead
+            scaled_h = min(height, LINES - 2);
+            scaled_w = static_cast<int>(static_cast<double>(scaled_h) / aspect_ratio);
+        }
+
+        resize(o_frame, rs_frame, Size(scaled_w, scaled_h));
+        cvtColor(rs_frame, f_frame, COLOR_BGR2GRAY);
+        
+        double fps = cap.get(CAP_PROP_FPS);
+        int delay = static_cast<int>(1000 / fps);
+        napms(delay);
+
+        curs_set(0);
+        clear();
+
+        // represent data in ascii and print to screen.
+        for (int y = 0; y < f_frame.rows; y++) {
+            for (int x = 0; x < f_frame.cols; x++) {
+                uchar pixel = f_frame.at<uchar>(y,x);
+                mvaddch(y, x, getAsciiChar(pixel));
+            }
+        }
+    
+        refresh();
+
+        nodelay(stdscr, TRUE);
+        if (getch() == 'q') break;
+    }
+    
+    
+    cap.release();
+    destroyAllWindows();
+    endwin();
+    return 0;
 }
